a72b30b6d7db1ef8cc6ca304f5c4541d2e6424cf
Helena Zhang <helenaz@stanford.edu>
2025-03-07 18:28:27 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pocketflow_framework",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Minimalist LLM Framework in 100 Lines. Enable LLMs to Program Themselves.",
+    url="https://github.com/The-Pocket-World/Pocketflow-Framework-Py",
+)
